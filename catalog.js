55ddb93377501468d95bd2b7ae91dbcc3854ad64
alokshr <alokshrest@gmail.com>
2024-10-20 22:21:46 -0500

    Changed titles of htmls

--- catalog.js
+++ catalog.js
@@ -1,21 +1,20 @@
-document.addEventListener("DOMContentLoaded", loadHomeContent)
+document.addEventListener("DOMContentLoaded", loadHomeContent);
 
 /** Adds a card filled with given JSON data to a given carousel
- * 
+ *
  * JSON data must have:
  * - id
  * - name
  * - description
  * - image
  * - price
-*/
+ */
 function addCardToAlbum(data) {
+  // Creating card to add to carousel
+  let items = document.getElementById("product-container");
+  let card = document.createElement("div");
 
-    // Creating card to add to carousel
-    let items = document.getElementById("product-container");
-    let card = document.createElement("div");
-
-    card.innerHTML = `
+  card.innerHTML = `
     <div class="col">
             <div class="card shadow-sm">
               <img src="${data.image.url}" alt="Image of ${data.name}, ${data.image.author}\nSource: ${data.image.source}" class="mt-3 img-fluid img-thumbnail border-light border-3 bg-light-subtle">
@@ -32,22 +31,21 @@ function addCardToAlbum(data) {
             </div>
           </div>
           `;
-    card = card.firstElementChild;
+  card = card.firstElementChild;
 
-    items.appendChild(card);
+  items.appendChild(card);
 }
 
 function loadHomeContent() {
-    fetch("./data.json")
-    .then(response => response.json())
-    .then(data => {
-        
-        for (let i = 0; i < data.length; i++) {
-            console.log(data[i])
-            addCardToAlbum(data[i]);
-        }
+  fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        console.log(data[i]);
+        addCardToAlbum(data[i]);
+      }
     })
-    .catch(error => console.log(error));
-    
-    let carouselTopSelling = document.getElementById("carouselTopSelling");
+    .catch((error) => console.log(error));
+
+  let carouselTopSelling = document.getElementById("carouselTopSelling");
 }
